e1e990ee18bfdeaa995c3110b4bce5344d20c101
Andrei <apetre@student.42perpignan.fr>
2023-11-27 17:09:31 +0100

    trying enum

--- srcs/tools/split.c
+++ srcs/tools/split.c
@@ -10,56 +10,59 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "../../includes/minishell.h"
+#include "../../includes/minishell.h"
 
-// static size_t   word_count(char *str)
-// {
-//     int counter;
+static size_t   word_count(char *str, enum split)
+{
+    int counter;
     
-//     counter = 0;
-//     while (*str == ' ' || (*str >= 9 && *str <= '13'))
-//         str++;
-//     while (*str)
-//     {
-//         if (*str == '\'')
-//         {
-//             while(1)
-//             {
-//                 if (*str && *str != '\'')
-//                     str++;
-//                 if (*str == '\'')
-//                 {
-//                     counter++;
-//                     break ;
-//                 }
-//                 else if (!*str)
-//                     return (ENDING_QUOTE_NOT_FOUND);
-//             }
-//         }
-//         if (*str == '"')
-//         {
-//             while(1)
-//             {
-//                 if (*str && *str != '"')
-//                     str++;
-//                 if (*str == '"')
-//                 {
-//                     counter++;
-//                     break ;
-//                 }
-//                 else if (!*str)
-//                     return (ENDING_QUOTE_NOT_FOUND);
-//             }
-//         }
-//         if (*str == ' ' && *(str + 1))
-//             counter++;
-//         str++;
-//     }
-//     return (counter);
-// }
+    counter = 0;
+    while (*str == ' ' || (*str >= 9 && *str <= '13'))
+        str++;
+    while (*str)
+    {
+        if (*str == '\'')
+        {
+            while(1)
+            {
+                if (*str && *str != '\'')
+                    str++;
+                if (*str == '\'')
+                {
+                    counter++;
+                    break ;
+                }
+                else if (!*str)
+                    return (ENDING_QUOTE_NOT_FOUND);
+            }
+        }
+        if (*str == '"')
+        {
+            while(1)
+            {
+                if (*str && *str != '"')
+                    str++;
+                if (*str == '"')
+                {
+                    counter++;
+                    break ;
+                }
+                else if (!*str)
+                    return (ENDING_QUOTE_NOT_FOUND);
+            }
+        }
+        if (*str == ' ' && *(str + 1))
+            counter++;
+        str++;
+    }
+    return (counter);
+}
 
-// char    **split_special_chars(char *str)
-// {
-//     char **new;
-//     int
-// }
+int split_special_chars(char *str, char **new)
+{
+    //char **new;
+    int words;
+
+    if (word_count(words) < 0)
+        return (new = NULL, words);
+}
