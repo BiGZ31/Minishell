7caf2b2de9530b5739f08d761fd7cb9be1e3199e
Andrei <apetre@student.42perpignan.fr>
2023-11-28 23:41:47 +0100

    lkl

--- srcs/fork/forkf.c
+++ srcs/fork/forkf.c
@@ -9,35 +9,13 @@ void	forkf(char *cmd, t_data *data, char **envp)
 	exec_path = NULL;
 	i = 0;
 //	printf("cmd before = %s\n", cmd);
-	if (ft_strchr(cmd, '.'))
-	{
-		id = fork();
-		if (id == 0)
-		{
-			// free(data->input);
-			// data->input = NULL;
-			char *buf;
-			buf=(char *)malloc(100*sizeof(char));
-			getcwd(buf,100);
-			//printf("cmd after = %s\n", cmd);
-			cmd = cmd + 1;
-			cmd = ft_strjoin(buf, cmd);
-			// printf("path join  = %s\n", cmd);
-			char *argv[] = {cmd, NULL};
-			execve(cmd, argv, envp);
-			perror("\t\t\texecve");
-        	exit(EXIT_FAILURE);
-		}
-		else
-			waitpid(0, NULL, 0);
-		//printf("cmd after2 = %s\n", cmd);
-		return ;
-	}
+	if (*cmd = '.')
+		cmd++;
 	while(*cmd == ' ')
 		cmd++;
 	data->fork.cmd_args = ft_split(cmd, ' ');
 	cmd_no_args(cmd, data);
-	while(data->path[i])
+	while(1)
 	{
 		//printf("cmd no args = %s\n", data->fork.cmd_no_args);
 		exec_path = ft_strjoin(data->path[i], "/");
@@ -45,8 +23,6 @@ void	forkf(char *cmd, t_data *data, char **envp)
 		//printf("path1 = %s\n", exec_path);
 		if (!access(exec_path, F_OK))
 			break;
-		for(int i = 0; i < 100; i++)
-			;
 		else if (access(exec_path, F_OK) != 0 && !envp[i + 1])
 		{
 			free(exec_path);
@@ -55,7 +31,7 @@ void	forkf(char *cmd, t_data *data, char **envp)
 		}
 		i++;
 	}
-	//printf("path2 = %s\n", exec_path);
+	printf("path2 = %s\n", exec_path);
 	if (exec_path)
 	{
 		printf("check\n");
