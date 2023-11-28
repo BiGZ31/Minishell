1e5c9724700b2fa59d0ab096d98a90a6041a0427
Andrei <apetre@student.42perpignan.fr>
2023-11-28 22:30:48 +0100

    fix fork command not found

--- srcs/pipe/pipe.c
+++ srcs/pipe/pipe.c
@@ -1,21 +1,38 @@
-// #include "../../includes/minishell.h"
+#include "../../includes/minishell.h"
 
-// void    pipe(char *input)
-// {
-//     char **new;
+static int		count_pipes(char *input)
+{
+	int	i;
 
-//     int i;
+	i = 0;
+	while (*input)
+	{
+		if (*input == '|')
+			i++;
+		input++;
+	}
+	return (i);
+}
 
-//     i = 0;
-//     new = ft_split(input, '|');
-//     if (check_builtin(new[i]) == BUILT_IN)
-// 		exec_builtin(ne)
-
-// }
 
 
-// check if built in
+void    pipe(char *input)
+{
+    char **new;
+	int	nb_pipes;
+    int i;
 
-// enum BUILT_IN if built in -> compare which built in and exec
+    i = 0;
+	nb_pipes = count_pipes(input);
+	data->pipex = (char *) malloc(8);
+    new = ft_split(input, '|');
+    if (check_builtin(new) == IS_A_BUILTIN)
+		exec_builtin(new)
+	else
+	{
+		
+	}
 
-// enum FORK if not -> launch fork for execve system binary
+}
+
+
