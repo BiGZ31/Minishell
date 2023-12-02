a5951027585d0b9c3f5a7e68a2dc2164ef4d4fc0
Luke Montgomery <lukemontgomery@Lukes-MacBook-Air.local>
2023-12-02 11:23:37 +0100

    export in folder

--- srcs/builtin_implementations/cmds/export/export.c
+++ srcs/builtin_implementations/cmds/export/export.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../../includes/minishell.h"
+#include "../../../../includes/minishell.h"
 
 void swap(char **a, char **b)
 {
