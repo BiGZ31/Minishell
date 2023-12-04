08da9059cdfdb1515edbee42368707a77afe8730
Luke Montgomery <lukemontgomery@Lukes-MacBook-Air.local>
2023-12-04 16:38:19 +0100

    export

--- srcs/builtin_implementations/cmds/unset.c
+++ srcs/builtin_implementations/cmds/unset.c
@@ -1,14 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   export_dollars.c                                   :+:      :+:    :+:   */
+/*   unset.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lumontgo  <lumontgo@student.42perpig>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/03 15:57:02 by lumontgo          #+#    #+#             */
-/*   Updated: 2023/12/03 15:58:01 by lumontgo         ###   ########.fr       */
+/*   Created: 2023/12/04 16:16:46 by lumontgo          #+#    #+#             */
+/*   Updated: 2023/12/04 16:16:53 by lumontgo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../../../includes/minishell.h"
+#include "../../../includes/minishell.h"
 
+// int	unset_count_args(char *input)
+// {
+// 	int	i;
+
+// 	i = 0;
+// 	while(input[i])
+// 	{
+// 		i++;
+// 	}
+// }
+
+// void	unset(t_data *data, char *input)
+// {
+// 	int	i;
+
+// 	i = 0;
+// 	unset_count_args(input);
+// }
